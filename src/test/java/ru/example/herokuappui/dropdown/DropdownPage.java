2181fdebc59deb0e4916f3a4b9ec5f1d80f7f241
Dmitrii Bogoslovskii <vrubel@labma.ru>
2025-03-06 19:41:05 +0300

    Small changes in Dropdown Page Object:
    - Added annotations for Allure Report:
    * @As for Elements.
    * @Step for methods.

---
+++ src/test/java/ru/example/herokuappui/dropdown/DropdownPage.java
@@ -0,0 +1,32 @@
+package ru.example.herokuappui.dropdown;
+
+import com.codeborne.selenide.As;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DropdownPage {
+
+    @As("Dropdown menu")
+    private SelenideElement dropdown = $("#dropdown");
+    @As("Selected option")
+    private SelenideElement selectedOption = $("#dropdown option:checked");
+
+    @Step("Select option in dropdown menu")
+    public void selectOption(String value) {
+        dropdown.selectOptionByValue(value);
+    }
+
+    @Step("Getting selected option")
+    public String getSelectedOption() {
+        return selectedOption.shouldBe(visible).getText();
+    }
+
+    @Step("Verify, that dropdown menu is visible")
+    public boolean isDropdownVisible() {
+        return dropdown.isDisplayed();
+    }
+}
+
---
+++ src/test/java/ru/example/herokuappui/dropdown/DropdownTest.java
@@ -0,0 +1,39 @@
+package ru.example.herokuappui.dropdown;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import utils.BaseDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DropdownTest {
+    private static DropdownPage dropdownPage;
+
+    @BeforeAll
+    public static void init() {
+        BaseDriver.setup();
+        dropdownPage = new DropdownPage();
+    }
+
+    @Test
+    public void testSelectOption() {
+        BaseDriver.openPage("/dropdown");
+        assertTrue(dropdownPage.isDropdownVisible(), "Dropdown should be visible");
+        dropdownPage.selectOption("1");
+        assertEquals("Option 1", dropdownPage.getSelectedOption(), "Selected option should be Option 1");
+    }
+
+    @Test
+    public void testSelectMultipleOptions() {
+        BaseDriver.openPage("/dropdown");
+        dropdownPage.selectOption("2");
+        assertEquals("Option 2", dropdownPage.getSelectedOption(), "Selected option should be Option 2");
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        BaseDriver.tearDown();
+    }
+}
