a4b4628f5128d0ef1ed659574ef60c48cc8915fd
Dmitrii Bogoslovskii <vrubel@labma.ru>
2025-03-06 16:34:29 +0300

    Context menu tests added.

---
+++ src/test/java/ru/example/herokuappui/contextmenu/ContextMenuPage.java
@@ -0,0 +1,32 @@
+package ru.example.herokuappui.contextmenu;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ContextMenuPage {
+
+    private SelenideElement hotSpot = $("#hot-spot");
+
+    @Step("Perform right click on hot-spot")
+    public void rightClickHotSpot() {
+        actions().contextClick(hotSpot).perform();
+    }
+
+    @Step("Getting alert text")
+    public String getAlertText() {
+        return switchTo().alert().getText(); // Получаем текст алерта
+    }
+
+    @Step("Accpeting alert window")
+    public void acceptAlert() {
+        switchTo().alert().accept(); // Подтверждаем алерт
+    }
+
+    @Step("Verify, that hot-spot is visible")
+    public boolean isHotSpotVisible() {
+        return hotSpot.shouldBe(visible).isDisplayed();
+    }
+}
---
+++ src/test/java/ru/example/herokuappui/contextmenu/ContextMenuTest.java
@@ -0,0 +1,43 @@
+package ru.example.herokuappui.contextmenu;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ru.example.herokuappui.common.CommonElementsPage;
+import utils.BaseDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContextMenuTest {
+    private static ContextMenuPage contextMenuPage;
+    private static CommonElementsPage cep;
+
+    @BeforeAll
+    public static void init() {
+        BaseDriver.setup();
+        contextMenuPage = new ContextMenuPage();
+        cep = new CommonElementsPage();
+    }
+
+    @Test
+    public void testPageLoadAndHotSpotVisibility() {
+        BaseDriver.openPage("/context_menu");
+        assertTrue(cep.isPageHeaderVisible(), "Page header should be visible");
+        assertTrue(contextMenuPage.isHotSpotVisible(), "Hot spot should be visible");
+    }
+
+    @Test
+    public void testContextMenuAndAlert() {
+        BaseDriver.openPage("/context_menu");
+        contextMenuPage.rightClickHotSpot();
+        String alertText = contextMenuPage.getAlertText();
+        assertEquals("You selected a context menu", alertText, "Alert text should match");
+        contextMenuPage.acceptAlert();
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        BaseDriver.tearDown();
+    }
+}
